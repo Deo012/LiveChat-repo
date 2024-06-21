418d1e151798ef712d98d8f86206fb9368b5e81d
Odjoumani Tayo Déo-Gracias Giscar-Bonaventure <deogracias.odjoumani@gmail.com>
2024-06-21 17:56:48 -0400

    Backend can create and retreive data

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express"); //an http API
 const cors = require("cors"); //allow to call the web site from anywhere
+const mongoose = require("mongoose")
 
 const app = express();
 app.use(express.json());
@@ -7,5 +8,20 @@ app.use(cors({ origin: true}));
 
 app.post("/authenticate", async (req, res) =>{
     const {username} = req.body;
-    return res.json()
+    return res.json({username: username, secret: "sha256..."})
+});
+
+const uri = "mongodb://localhost:27017/dbLiveChat";
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () =>{
+    console.log("MongoDB connected sucessfully");
+})
+connection.on("error", console.error.bind(console, "MongoDB connection error: "))
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+
+app.listen(3001, ()=>{
+    console.log("Serveur is running on port 3001");
 });
